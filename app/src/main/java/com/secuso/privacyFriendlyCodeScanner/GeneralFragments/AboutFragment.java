085fd50c7aaf585ff6b932ec01604c34b30adcef
Karola <yonjuni@pinyto.de>
2016-12-27 02:46:05 +0200

    VersionName in Aboutpage set programmatically part 2

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/GeneralFragments/AboutFragment.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/GeneralFragments/AboutFragment.java
@@ -1,5 +1,6 @@
 package com.secuso.privacyFriendlyCodeScanner.GeneralFragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
@@ -8,12 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.secuso.privacyFriendlyCodeScanner.BuildConfig;
 import com.secuso.privacyFriendlyCodeScanner.R;
 
 /**
  * Created by Philipp on 28.09.2015.
  */
 public class AboutFragment extends Fragment {
+
+    private Activity activity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,7 +27,16 @@ public class AboutFragment extends Fragment {
 
         ((TextView)rootView.findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
         ((TextView)rootView.findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView)rootView.findViewById(R.id.textFieldVersionName)).setText(BuildConfig.VERSION_NAME);
+
 
         return rootView;
     }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.activity = activity;
+    }
+
 }
