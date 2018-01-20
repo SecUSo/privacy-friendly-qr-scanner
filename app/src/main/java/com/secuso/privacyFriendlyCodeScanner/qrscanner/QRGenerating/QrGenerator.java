b331ab5d2a4153f1c32d2111afafafc27fb91b12
bassel-hanna <32960820+bassel-hanna@users.noreply.github.com>
2018-01-20 01:04:29 +0100

    1- QrGenerator activity design has been modified. 2- add new activities:     -MailEnterActivity     -SmsEnterActivity     -GeoEnterActivity     -TelEnterActivity     -UrlEnterActivity

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/QRGenerating/QrGenerator.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/QRGenerating/QrGenerator.java
@@ -5,14 +5,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.secuso.privacyFriendlyCodeScanner.qrscanner.History;
 import com.secuso.privacyFriendlyCodeScanner.qrscanner.MainActivity;
 import com.secuso.privacyFriendlyCodeScanner.qrscanner.R;
+import com.secuso.privacyFriendlyCodeScanner.qrscanner.ResultActivity;
 import com.secuso.privacyFriendlyCodeScanner.qrscanner.ResultsActivities.MmsActivity;
 import com.secuso.privacyFriendlyCodeScanner.qrscanner.ResultsActivities.SmsActivity;
 import com.secuso.privacyFriendlyCodeScanner.qrscanner.ResultsActivities.TextActivity;
@@ -21,7 +27,7 @@ import com.secuso.privacyFriendlyCodeScanner.qrscanner.ResultsActivities.TextAct
 public class QrGenerator extends AppCompatActivity {
 
 
-
+    ListView listView;
 
 
     @SuppressLint("RestrictedApi")
@@ -31,129 +37,55 @@ public class QrGenerator extends AppCompatActivity {
         setContentView(R.layout.activity_qr_generator);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
-        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        listView = (ListView) findViewById(R.id.list_view);
 
-        final RadioButton btnMail = (RadioButton) findViewById(R.id.radioMail);
-        final RadioButton btnTel = (RadioButton) findViewById(R.id.radioTel);
-        final RadioButton btnUrl = (RadioButton) findViewById(R.id.radioUrl);
-        final RadioButton btnSms = (RadioButton) findViewById(R.id.radioSms);
-        final RadioButton btnGeo = (RadioButton) findViewById(R.id.radioGeo);
+        String[] generator = getResources().getStringArray(R.array.generator_array);
+        //Row layout defined by Android: android.R.layout.simple_list_item_1
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, generator));
+       // listView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
 
-        final EditText qrResult=(EditText) findViewById(R.id.gnResult);
-        final EditText qrSms=(EditText) findViewById(R.id.editTel);
-        final EditText qrText=(EditText) findViewById(R.id.editText1);
-        final EditText qrLatitude=(EditText) findViewById(R.id.editGeo1);
-        final EditText qrLongitude=(EditText) findViewById(R.id.editGeo2);
-
-        Button generate=(Button) findViewById(R.id.generate);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // checkedId is the RadioButton selected
-                switch (checkedId)
-                {
-                    case R.id.radioMail:
-                        qrResult.setEnabled(true);
-                        qrSms.setEnabled(false);
-                        qrText.setEnabled(false);
-                        qrLatitude.setEnabled(false);
-                        qrLongitude.setEnabled(false);
-                        break;
-
-                    case R.id.radioTel:
-                        qrResult.setEnabled(true);
-                        qrSms.setEnabled(false);
-                        qrText.setEnabled(false);
-                        qrLatitude.setEnabled(false);
-                        qrLongitude.setEnabled(false);
-                        break;
-
-                    case R.id.radioUrl:
-                        qrResult.setEnabled(true);
-                        qrSms.setEnabled(false);
-                        qrText.setEnabled(false);
-                        qrLatitude.setEnabled(false);
-                        qrLongitude.setEnabled(false);
-                        break;
-
-                    case R.id.radioSms:
-                        qrResult.setEnabled(false);
-                        qrLatitude.setEnabled(false);
-                        qrLongitude.setEnabled(false);
-                        qrSms.setEnabled(true);
-                        qrText.setEnabled(true);
-                        break;
-
-                    case R.id.radioGeo:
-                        qrResult.setEnabled(false);
-                        qrSms.setEnabled(false);
-                        qrText.setEnabled(false);
-                        qrLatitude.setEnabled(true);
-                        qrLongitude.setEnabled(true);
-                        break;
-                }
-
-            }
-        });
-
-
-
-        generate.setOnClickListener(new View.OnClickListener() {
-            String result;
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-                // find which radioButton is checked by id
-
-                if(selectedId == btnMail.getId()) {
-
-                    result = qrResult.getText().toString();
-                    Intent i = new Intent(QrGenerator.this, MailGnrActivity.class);
-                    i.putExtra("gn", result);
-                    startActivity(i);
-
-                } else if(selectedId == btnTel.getId()) {
-
-                    result = qrResult.getText().toString();
-                    Intent i = new Intent(QrGenerator.this, TelGnrActivity.class);
-                    i.putExtra("gn", result);
-                    startActivity(i);
-
-
-                }  else if(selectedId == btnUrl.getId()) {
-
-                    result = qrResult.getText().toString();
-                    Intent i = new Intent(QrGenerator.this, UrlGnrActivity.class);
-                    i.putExtra("gn", result);
-                    startActivity(i);
-                } else if (selectedId==btnSms.getId()){
-
-                    result = qrSms.getText().toString()+":"+qrText.getText().toString();
-                    Intent i = new Intent(QrGenerator.this, SmsGnrActivity.class);
-                    i.putExtra("gn", result);
-                    startActivity(i);
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    Intent appInfo = new Intent(QrGenerator.this, MailEnterActivity.class);
+                    startActivity(appInfo);
                 }
-                else if (selectedId==btnGeo.getId()){
-
-                   result = qrLatitude.getText().toString()+","+qrLongitude.getText().toString();
-
-
-                    Intent i = new Intent(QrGenerator.this, GeoLocatioGnrActivity.class);
-                    i.putExtra("gn", result);
-                    startActivity(i);
-
+                if(position==1){
+                    Intent english=new Intent(QrGenerator.this,UrlEnterActivity.class);
+                    startActivity(english);
                 }
+                if(position==2){
+                    Intent english=new Intent(QrGenerator.this,TelEnterActivity.class);
+                    startActivity(english);
+                }
+                if(position==3){
+                    Intent english=new Intent(QrGenerator.this,SmsEnterActivity.class);
+                    startActivity(english);
+                }
+
+                if(position==4){
+                    Intent english=new Intent(QrGenerator.this,GeoLocationEnterActivity.class);
+                    startActivity(english);
+                }
+
+                if(position==5){
+                    Intent english=new Intent(QrGenerator.this,TelEnterActivity.class);
+                    startActivity(english);
+                }
+
+                if(position==6){
+                    Intent english=new Intent(QrGenerator.this,TelEnterActivity.class);
+                    startActivity(english);
+                }
+
 
             }
-
         });
-
-
-
 
     }
+
 
 
 }
