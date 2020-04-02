a2a4d25799717419a05d6a5866374054b2a16632
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 21:54:04 +0200

    Update AppDatabase.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/DataBase/AppDatabase.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/DataBase/AppDatabase.java
@@ -1,15 +1,15 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.database;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
-import android.arch.persistence.room.TypeConverters;
-import android.arch.persistence.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import androidx.room.migration.Migration;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.google.zxing.BarcodeFormat;
 import com.secuso.privacyfriendlycodescanner.qrscanner.helpers.Utils;
