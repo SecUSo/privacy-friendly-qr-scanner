c41be4ebfb1efd93798b2457fd4919f015cde0be
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 23:46:46 +0200

    Update SplashActivity.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/activities/SplashActivity.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/activities/SplashActivity.java
@@ -1,7 +1,7 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.secuso.privacyfriendlycodescanner.qrscanner.helpers.PrefManager;
 
