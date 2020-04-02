f37f4e3381a8f728df6441b082ab9475c7b24370
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 23:53:29 +0200

    Update HistoryAdapter.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/adapter/HistoryAdapter.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/adapter/HistoryAdapter.java
@@ -1,10 +1,10 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.adapter;
 
 import android.content.Context;
-import android.databinding.DataBindingUtil;
+import androidx.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
