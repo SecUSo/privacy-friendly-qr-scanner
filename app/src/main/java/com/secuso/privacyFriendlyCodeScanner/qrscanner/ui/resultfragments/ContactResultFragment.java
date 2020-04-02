74044f6030b215c90bcb02a539ef2ab24618fcca
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 23:57:21 +0200

    Update ContactResultFragment.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/resultfragments/ContactResultFragment.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/resultfragments/ContactResultFragment.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,3 +71,4 @@ public class ContactResultFragment extends ResultFragment {
         builder.create().show();
     }
 }
+
