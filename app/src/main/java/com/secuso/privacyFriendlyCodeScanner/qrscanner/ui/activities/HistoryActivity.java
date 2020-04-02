e7382dc897f692063532e44c1c274664b836326c
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 23:31:39 +0200

    Update HistoryActivity.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/activities/HistoryActivity.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/activities/HistoryActivity.java
@@ -1,12 +1,12 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities;
 
 import android.app.AlertDialog;
-import android.arch.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -74,4 +74,3 @@ public class HistoryActivity extends AppCompatActivity {
 
 
 }
-
