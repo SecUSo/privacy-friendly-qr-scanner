54c76b6e92179f98fa4b3809b438fb9c35a30420
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 22:07:23 +0200

    Update MailEnterActivity.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/activities/generator/MailEnterActivity.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/activities/generator/MailEnterActivity.java
@@ -1,7 +1,7 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities.generator;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.view.View;
