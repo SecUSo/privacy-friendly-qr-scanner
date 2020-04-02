106635f766e6dbad1f6b8277b42ec04db9ea9a4d
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 23:51:13 +0200

    Update EmailResultAdapter.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/adapter/EmailResultAdapter.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/adapter/EmailResultAdapter.java
@@ -1,136 +1,90 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.adapter;
 
 import android.content.Context;
+import androidx.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
 import androidx.annotation.NonNull;
-import androidx.annotation.StringRes;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.google.zxing.client.result.EmailAddressParsedResult;
+import com.bumptech.glide.Glide;
 import com.secuso.privacyfriendlycodescanner.qrscanner.R;
+import com.secuso.privacyfriendlycodescanner.qrscanner.database.HistoryItem;
+import com.secuso.privacyfriendlycodescanner.qrscanner.databinding.ItemHistoryCodeBinding;
+import com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities.HistoryActivity;
+import com.secuso.privacyfriendlycodescanner.qrscanner.ui.viewmodel.HistoryItemViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmailResultAdapter extends RecyclerView.Adapter<EmailResultAdapter.EmailViewHolder> {
+/**
+ * Adapter for the RecyclerView in {@link HistoryActivity}.
+ *
+ * @author Christopher Beckmann
+ */
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryItemViewHolder> {
 
-    public static class EmailResultItem {
-        public final EmailResultItemType type;
-        public final String content;
+    private Context context;
+    private List<HistoryItem> historyEntries;
 
-        EmailResultItem(EmailResultItemType type, String content) {
-            this.type = type;
-            this.content = content;
-        }
+    public HistoryAdapter(Context context) {
+        this.context = context;
+        this.historyEntries = new ArrayList<>();
+
+        setHasStableIds(true);
     }
 
-    public enum EmailResultItemType {
-        TYPE_TO(R.string.item_result_email_to),
-        TYPE_CC(R.string.item_result_email_cc),
-        TYPE_BCC(R.string.item_result_email_bcc),
-        TYPE_SUBJECT(R.string.item_result_email_subject),
-        TYPE_BODY(R.string.item_result_email_body);
-
-        @StringRes int local;
-
-        EmailResultItemType(int local) {
-            this.local = local;
-        }
-    }
-
-    private List<EmailResultItem> resultItems = new ArrayList<>();
-
-    private EmailResultAdapter(@NonNull List<EmailResultItem> resultItems) {
-        this.resultItems.addAll(resultItems);
-    }
-
-    public EmailResultAdapter(EmailAddressParsedResult result) {
-        this(buildResultItems(result));
-    }
-
-    private static List<EmailResultItem> buildResultItems(EmailAddressParsedResult result) {
-        List<EmailResultItem> items = new ArrayList<>();
-
-        if(result != null) {
-            if(result.getTos() != null) {
-                for(String to : result.getTos()) {
-                    items.add(new EmailResultItem(EmailResultItemType.TYPE_TO, to));
-                }
-            }
-            if(result.getCCs() != null) {
-                for(String cc : result.getCCs()) {
-                    items.add(new EmailResultItem(EmailResultItemType.TYPE_CC, cc));
-                }
-            }
-            if(result.getBCCs() != null) {
-                for(String bcc : result.getBCCs()) {
-                    items.add(new EmailResultItem(EmailResultItemType.TYPE_BCC, bcc));
-                }
-            }
-            if(result.getSubject() != null) {
-                items.add(new EmailResultItem(EmailResultItemType.TYPE_SUBJECT, result.getSubject()));
-            }
-            if(result.getBody() != null) {
-                items.add(new EmailResultItem(EmailResultItemType.TYPE_BODY, result.getBody()));
-            }
-        }
-        return items;
+    public void setHistoryEntries(List<HistoryItem> entries) {
+        historyEntries.clear();
+        historyEntries.addAll(entries);
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
-    public EmailViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-        View v = inflater.inflate(R.layout.item_result_email, viewGroup, false);
-//        switch(EmailResultItemType.values()[viewType]) {
-//            case TYPE_CC:
-//                v = inflater.inflate(R.layout.item_result_email, viewGroup, false);
-//                break;
-//            case TYPE_BCC:
-//                v = inflater.inflate(R.layout.item_result_email, viewGroup, false);
-//                break;
-//            case TYPE_SUBJECT:
-//                v = inflater.inflate(R.layout.item_result_email, viewGroup, false);
-//                break;
-//            case TYPE_BODY:
-//                v = inflater.inflate(R.layout.item_result_email, viewGroup, false);
-//                break;
-//            case TYPE_TO: default:
-//                v = inflater.inflate(R.layout.item_result_email, viewGroup, false);
-//                break;
-//
-//        }
-        return new EmailViewHolder(v);
+    public HistoryItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ItemHistoryCodeBinding binding = DataBindingUtil.inflate(
+                LayoutInflater.from(viewGroup.getContext()),
+                R.layout.item_history_code,
+                viewGroup,
+                false
+        );
+        return new HistoryItemViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull EmailViewHolder viewHolder, int i) {
-        Context context = viewHolder.content.getContext();
-        viewHolder.content.setText(resultItems.get(i).content);
-        viewHolder.type.setText(context.getString(resultItems.get(i).type.local));
+    public void onBindViewHolder(@NonNull HistoryItemViewHolder historyItemViewHolder, int i) {
+        ItemHistoryCodeBinding binding = historyItemViewHolder.binding;
+        HistoryItemViewModel viewModel = new HistoryItemViewModel(context, historyEntries.get(i));
+        binding.setViewModel(viewModel);
+        binding.itemView.setOnClickListener(viewModel.onClickItem());
+        binding.itemView.setOnLongClickListener(viewModel.onLongClickItem());
+
+        Bitmap image = historyEntries.get(i).getImage();
+        if(image != null) {
+            Glide.with(context).load(image).placeholder(R.drawable.ic_no_image_accent_24dp).into(binding.itemHistoryImage);
+        } else {
+            Glide.with(context).load(R.drawable.ic_no_image_accent_24dp).into(binding.itemHistoryImage);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return resultItems.size();
+        return historyEntries.size();
     }
 
     @Override
-    public int getItemViewType(int position) {
-        return resultItems.get(position).type.ordinal();
+    public long getItemId(int i) {
+        return historyEntries.get(i).get_id();
     }
 
-    class EmailViewHolder extends RecyclerView.ViewHolder {
-        TextView content;
-        TextView type;
-        EmailViewHolder(@NonNull View itemView) {
-            super(itemView);
-            content = itemView.findViewById(R.id.item_result_email_content);
-            type = itemView.findViewById(R.id.item_result_email_type);
+    public static class HistoryItemViewHolder extends RecyclerView.ViewHolder {
+        ItemHistoryCodeBinding binding;
+
+        public HistoryItemViewHolder(@NonNull ItemHistoryCodeBinding binding) {
+            super(binding.itemView);
+            this.binding = binding;
         }
     }
 }
-
