4d4d01ab1fc0e8697d30aee932a79c809fb45dcb
htes9 <7214961+htes9@users.noreply.github.com>
2020-04-02 23:59:33 +0200

    Update ResultFragment.java

--- app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/resultfragments/ResultFragment.java
+++ app/src/main/java/com/secuso/privacyFriendlyCodeScanner/qrscanner/ui/resultfragments/ResultFragment.java
@@ -1,11 +1,11 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.resultfragments;
 
-import android.arch.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.google.zxing.client.result.ParsedResult;
 import com.secuso.privacyfriendlycodescanner.qrscanner.ui.viewmodel.ResultViewModel;
@@ -44,3 +44,4 @@ public abstract class ResultFragment extends Fragment {
 
     public abstract void onProceedPressed(Context context);
 }
+
