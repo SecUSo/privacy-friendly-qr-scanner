6b2b7c185e95a46b8d5be075478850c422fda735
Tobias Länge <tobias.laenge9@kit.edu>
2022-05-23 16:51:03 +0200

    Update url generator layout

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/generator/UrlEnterActivity.java
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/generator/UrlEnterActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.secuso.privacyfriendlycodescanner.qrscanner.R;
 import com.secuso.privacyfriendlycodescanner.qrscanner.generator.Contents;
 
@@ -20,14 +21,24 @@ public class UrlEnterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_url_enter);
 
-        final EditText qrResult=(EditText) findViewById(R.id.gnResult);
-        Button generate=(Button) findViewById(R.id.generate);
+        final EditText qrResult = (EditText) findViewById(R.id.editURL);
+        final TextInputLayout inputLayout = (TextInputLayout) findViewById(R.id.editURLInputLayout);
+        Button generate = (Button) findViewById(R.id.btnGenerate);
+
+        // Enable text wrapping without line breaks.
+        // (Only works if it is set via the code and not via the xml):
+        qrResult.setMaxLines(Integer.MAX_VALUE);
+        qrResult.setHorizontallyScrolling(false);
+
+        // Set Input field helper text:
+        inputLayout.setHelperText(getString(R.string.generator_url_explanation) + " " + getString(R.string.generator_example_url));
 
         int maxLength = 150;
-        qrResult.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
+        qrResult.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
 
         generate.setOnClickListener(new View.OnClickListener() {
             String result;
+
             @Override
             public void onClick(View v) {
                 result = qrResult.getText().toString();
