764a374143cc9c906fcbe9d9c907e25220c3fe37
Tobias Länge <tobias.laenge9@kit.edu>
2022-05-27 16:38:23 +0200

    Update history list
    - Adjust layout
    - Add code type image
    - Replace QR-code image with content type icon

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/adapter/HistoryAdapter.java
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/adapter/HistoryAdapter.java
@@ -1,18 +1,20 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.adapter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.zxing.client.result.ResultParser;
 import com.secuso.privacyfriendlycodescanner.qrscanner.R;
 import com.secuso.privacyfriendlycodescanner.qrscanner.database.HistoryItem;
 import com.secuso.privacyfriendlycodescanner.qrscanner.databinding.ItemHistoryCodeBinding;
+import com.secuso.privacyfriendlycodescanner.qrscanner.generator.Contents;
 import com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities.HistoryActivity;
 import com.secuso.privacyfriendlycodescanner.qrscanner.ui.viewmodel.HistoryItemViewModel;
 
@@ -56,18 +58,26 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryI
 
     @Override
     public void onBindViewHolder(@NonNull HistoryItemViewHolder historyItemViewHolder, int i) {
+        HistoryItem historyItem = historyEntries.get(i);
         ItemHistoryCodeBinding binding = historyItemViewHolder.binding;
-        HistoryItemViewModel viewModel = new HistoryItemViewModel(context, historyEntries.get(i));
+        HistoryItemViewModel viewModel = new HistoryItemViewModel(context, historyItem);
         binding.setViewModel(viewModel);
         binding.itemView.setOnClickListener(viewModel.onClickItem());
         binding.itemView.setOnLongClickListener(viewModel.onLongClickItem());
 
-        Bitmap image = historyEntries.get(i).getImage();
-        if(image != null) {
-            Glide.with(context).load(image).placeholder(R.drawable.ic_no_image_accent_24dp).into(binding.itemHistoryImage);
-        } else {
-            Glide.with(context).load(R.drawable.ic_no_image_accent_24dp).into(binding.itemHistoryImage);
+        Contents.Type contentType = Contents.Type.parseParsedResultType(ResultParser.parseResult(historyItem.getResult()).getType());
+        Glide.with(context).load(contentType.getIcon()).placeholder(R.drawable.ic_no_image_accent_24dp).into(binding.itemHistoryImage);
+
+        @DrawableRes Integer codeTypeDrawableRes;
+        switch (historyItem.getFormat()) {
+            case QR_CODE:
+                codeTypeDrawableRes = R.drawable.ic_baseline_qr_code_24dp;
+                break;
+            default:
+                codeTypeDrawableRes = R.drawable.ic_barcode_24dp;
+                break;
         }
+        Glide.with(context).load(codeTypeDrawableRes).placeholder(R.drawable.ic_no_image_accent_24dp).into(binding.itemHistoryTypeImage);
     }
 
     @Override
