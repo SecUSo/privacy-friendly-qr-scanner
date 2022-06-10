e4e6697b57f546c3189b68ccde652be3a5b94f40
Tobias Länge <tobias.laenge9@kit.edu>
2022-06-10 16:47:21 +0200

    Small design changes in HistoryActivity
    - Add ripple effect to card view
    - Change color of alert dialog icon to red

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/HistoryActivity.java
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/HistoryActivity.java
@@ -1,12 +1,15 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities;
 
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -54,25 +57,27 @@ public class HistoryActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_clear:
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.D_all)
-                        .setMessage(R.string.all_records)
-                        .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                AppRepository.getInstance(HistoryActivity.this).deleteAllHistoryEntries();
-                            }
-                        })
-                        .setNegativeButton(android.R.string.cancel, null)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .create().show();
-                return true;
-            default:
-                return false;
+        if (item.getItemId() == R.id.action_clear) {
+            Drawable icon = AppCompatResources.getDrawable(getApplicationContext(), android.R.drawable.ic_dialog_alert);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                if (icon != null) {
+                    icon.setTint(getResources().getColor(R.color.red));
+                }
+            }
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.D_all)
+                    .setMessage(R.string.all_records)
+                    .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            AppRepository.getInstance(HistoryActivity.this).deleteAllHistoryEntries();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setIcon(icon)
+                    .create().show();
+            return true;
         }
+        return false;
     }
-
-
 }
 
