6ee591602d79056aaa4a1a248de214bd5c01e96a
Tobias Länge <tobias.laenge9@kit.edu>
2022-06-13 14:13:26 +0200

    Fix crash when scanning extended product information
    Fixes #124

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/resultfragments/ProductResultFragment.java
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/resultfragments/ProductResultFragment.java
@@ -9,50 +9,57 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
+import com.google.zxing.client.result.ExpandedProductParsedResult;
+import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.ProductParsedResult;
 import com.secuso.privacyfriendlycodescanner.qrscanner.R;
 
 public class ProductResultFragment extends ResultFragment {
-
-    ProductParsedResult result;
 
     public ProductResultFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View v = inflater.inflate(R.layout.fragment_result_product, container, false);
 
-        result = (ProductParsedResult) parsedResult;
-
         TextView resultText = v.findViewById(R.id.fragment_result_product_content);
 
-        resultText.setText(result.getProductID());
+        resultText.setText(getProductID(parsedResult));
 
         return v;
+    }
+
+    private String getProductID(ParsedResult parsedResult) {
+        if (parsedResult instanceof ProductParsedResult) {
+            ProductParsedResult result = (ProductParsedResult) parsedResult;
+            return result.getProductID();
+        } else if (parsedResult instanceof ExpandedProductParsedResult) {
+            ExpandedProductParsedResult result = (ExpandedProductParsedResult) parsedResult;
+            return result.getProductID();
+        } else {
+            return parsedResult.getDisplayResult();
+        }
     }
 
     public void onProceedPressed(Context context) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle(R.string.choose_action)
                 .setItems(R.array.product_array, (dialog, which) -> {
-                    switch (which) {
-                        case 0:
-                            // TODO: ?! I don't see this working..
-                            String url = result.getProductID();
-                            Intent intent = new Intent(Intent.ACTION_VIEW);
-                            intent.setData(Uri.parse(url));
+                    if (which == 0) {
+                        // TODO: ?! I don't see this working..
+                        String url = getProductID(parsedResult);
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(url));
 
-                            String caption = getResources().getStringArray(R.array.product_array)[0];
-                            startActivity(Intent.createChooser(intent, caption));
-                            break;
-
-                        default:
+                        String caption = getResources().getStringArray(R.array.product_array)[0];
+                        startActivity(Intent.createChooser(intent, caption));
                     }
                 });
         builder.create().show();
