cf30dfa8c1b78e2970650e6dff9adf58b670b81d
Tobias Länge <tobias.laenge9@kit.edu>
2022-06-20 13:14:02 +0200

    Add icons to generator overview

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/generator/QrGeneratorOverviewActivity.java
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/ui/activities/generator/QrGeneratorOverviewActivity.java
@@ -11,6 +11,7 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.secuso.privacyfriendlycodescanner.qrscanner.R;
+import com.secuso.privacyfriendlycodescanner.qrscanner.ui.helpers.GeneratorListAdapter;
 
 
 public class QrGeneratorOverviewActivity extends AppCompatActivity {
@@ -26,10 +27,21 @@ public class QrGeneratorOverviewActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list_view);
 
         String[] generator = getResources().getStringArray(R.array.generator_array);
-        //Row layout defined by Android: android.R.layout.simple_list_item_1
-        listView.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, generator));
-       // listView.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+        Integer[] icons = new Integer[]{
+                R.drawable.ic_baseline_subject_24dp,
+                R.drawable.ic_email_accent_24dp,
+                R.drawable.ic_baseline_public_24dp,
+                R.drawable.ic_phone_accent_24dp,
+                R.drawable.ic_baseline_sms_24dp,
+                R.drawable.ic_baseline_place_24dp,
+                R.drawable.ic_person_accent_24dp,
+                R.drawable.ic_person_accent_24dp,
+                R.drawable.ic_baseline_wifi_24dp,
+                R.drawable.ic_person_accent_24dp,
+                R.drawable.ic_baseline_shopping_cart_24dp};
+
+        GeneratorListAdapter adapter = new GeneratorListAdapter(this, generator, icons);
+        listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -43,38 +55,37 @@ public class QrGeneratorOverviewActivity extends AppCompatActivity {
                         intent = new Intent(QrGeneratorOverviewActivity.this, MailEnterActivity.class);
                         break;
                     case 2:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,UrlEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, UrlEnterActivity.class);
                         break;
                     case 3:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,TelEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, TelEnterActivity.class);
                         break;
                     case 4:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,SmsEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, SmsEnterActivity.class);
                         break;
                     case 5:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,GeoLocationEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, GeoLocationEnterActivity.class);
                         break;
                     case 6:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,MeCardEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, MeCardEnterActivity.class);
                         break;
                     case 7:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,BizCardEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, BizCardEnterActivity.class);
                         break;
                     case 8:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,WifiEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, WifiEnterActivity.class);
                         break;
                     case 9:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,VcardEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, VcardEnterActivity.class);
                         break;
                     case 10:
-                        intent = new Intent(QrGeneratorOverviewActivity.this,MarketEnterActivity.class);
+                        intent = new Intent(QrGeneratorOverviewActivity.this, MarketEnterActivity.class);
                         break;
                 }
                 startActivity(intent);
             }
         });
     }
-
 
 
 }
