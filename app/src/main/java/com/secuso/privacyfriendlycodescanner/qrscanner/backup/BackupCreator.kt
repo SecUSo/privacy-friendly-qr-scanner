0ecc8c9852964bc2421d2c92c10addd382814bae
Tobias Länge <tobias.laenge9@kit.edu>
2022-06-22 16:07:18 +0200

    Add Backup API dependencies

---
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/backup/BackupCreator.kt
@@ -0,0 +1,11 @@
+package com.secuso.privacyfriendlycodescanner.qrscanner.backup
+
+import android.content.Context
+import org.secuso.privacyfriendlybackup.api.pfa.IBackupCreator
+import java.io.OutputStream
+
+class BackupCreator : IBackupCreator {
+    override fun writeBackup(context: Context, outputStream: OutputStream) {
+        TODO("Not yet implemented")
+    }
+}
