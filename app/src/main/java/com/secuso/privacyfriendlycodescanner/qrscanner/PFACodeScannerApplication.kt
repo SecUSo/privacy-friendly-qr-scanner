af2fb63effcdf54ee11ed3db124ef5a4cc37324d
Tobias Länge <tobias.laenge9@kit.edu>
2022-06-23 18:46:40 +0200

    Fix crash on lower API levels by switching to MultiDex

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/PFACodeScannerApplication.kt
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/PFACodeScannerApplication.kt
@@ -1,13 +1,13 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner
 
-import android.app.Application
 import android.util.Log
+import androidx.multidex.MultiDexApplication
 import androidx.work.Configuration
 import com.secuso.privacyfriendlycodescanner.qrscanner.backup.BackupCreator
 import com.secuso.privacyfriendlycodescanner.qrscanner.backup.BackupRestorer
 import org.secuso.privacyfriendlybackup.api.pfa.BackupManager
 
-class PFACodeScannerApplication : Application(), Configuration.Provider {
+class PFACodeScannerApplication : MultiDexApplication(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         BackupManager.backupCreator = BackupCreator()
