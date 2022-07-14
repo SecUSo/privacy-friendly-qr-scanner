6680da129aa7ab4d49812245c2397d5f04f7ae32
ColumbusUtrigas <columbusutrigas@gmail.com>
2022-07-14 20:03:00 +0400

    added quick settings launch button with icon

---
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/QuickTileService.kt
@@ -0,0 +1,19 @@
+package com.secuso.privacyfriendlycodescanner.qrscanner
+
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+import com.secuso.privacyfriendlycodescanner.qrscanner.ui.activities.ScannerActivity
+
+@RequiresApi(Build.VERSION_CODES.N)
+class QuickTileService : TileService() {
+    override fun onClick() {
+        super.onClick()
+
+        val intent = Intent(this, ScannerActivity::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        startActivityAndCollapse(intent)
+    }
+}
