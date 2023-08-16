45c67da208f98e1eccab1504c0e5701660acc00a
Tobias Länge <tobias.laenge@kit.edu>
2023-08-16 17:09:43 +0200

    Add setting to manually select dark/light mode
    - Update Preferences to andoridx.preferences
    - Add theme preference to BackupRestorer

--- app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/PFACodeScannerApplication.kt
+++ app/src/main/java/com/secuso/privacyfriendlycodescanner/qrscanner/PFACodeScannerApplication.kt
@@ -1,10 +1,13 @@
 package com.secuso.privacyfriendlycodescanner.qrscanner
 
+import android.preference.PreferenceManager
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import androidx.work.Configuration
 import com.secuso.privacyfriendlycodescanner.qrscanner.backup.BackupCreator
 import com.secuso.privacyfriendlycodescanner.qrscanner.backup.BackupRestorer
+import com.secuso.privacyfriendlycodescanner.qrscanner.helpers.PreferenceKeys
 import org.secuso.privacyfriendlybackup.api.pfa.BackupManager
 
 class PFACodeScannerApplication : MultiDexApplication(), Configuration.Provider {
@@ -12,10 +15,26 @@ class PFACodeScannerApplication : MultiDexApplication(), Configuration.Provider 
         super.onCreate()
         BackupManager.backupCreator = BackupCreator()
         BackupManager.backupRestorer = BackupRestorer()
+
+
+        when (PreferenceManager.getDefaultSharedPreferences(applicationContext)
+            .getString(PreferenceKeys.APP_THEME, getString(R.string.pref_app_theme_default))) {
+            "DARK" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+
+            "LIGHT" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            else -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder().setMinimumLoggingLevel(Log.INFO).build();
+        return Configuration.Builder().setMinimumLoggingLevel(Log.INFO).build()
     }
 
 }
